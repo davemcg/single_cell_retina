16a1714ee63e4c30932535888bbdc096993b1281
David McGaughey <mcgaughey@gmail.com>
2017-09-14 17:01:26 -0400

    gene cell assignment

---
+++ scripts/gene_pos_cell_type.R
@@ -0,0 +1,24 @@
+# gene+ cell assignment
+
+# takes the macosko cell classifications and returns, for each gene, the percentage of each of the 12 major cell types 
+load('data/retina_seurat_superSet.Rdata')
+source('scripts/macosko_cluster_assignments.R')
+
+library(Seurat)
+library(tidyverse)
+
+all_genes <- retina_superset@data %>% rownames()
+gene_type_counts <- data.frame(c('Amacrine cells','Astrocytes','Bipolar cells','Cones','Fibroblasts','Horizontal cells','Microglia','Muller glia','Pericytes','Retinal ganglion cells','Rods','Vascular endothelium'))
+colnames(gene_type_counts) <- 'Cell Type'
+                               
+for (i in all_genes){
+  gene_pos <- retina_superset@data[i,] > 0.1
+  df <- retina_superset@meta.data[gene_pos,'Cell Type'] %>% table() %>% as.data.frame() %>% select(Freq)
+  colnames(df) <- c(i)
+  gene_type_counts <- cbind(gene_type_counts, df)
+}
+
+
+row.names(gene_type_counts) <- gene_type_counts$c..Amacrine.cells....Astrocytes....Bipolar.cells....Cones....Fibroblasts...
+gene_type_counts <- gene_type_counts[,-1]
+save(gene_type_counts, file='data/gene_type_counts.Rdata')
