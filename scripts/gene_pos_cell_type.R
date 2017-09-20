c2be8a622946898f2986152a44799583ee50771d
David McGaughey <mcgaughey@gmail.com>
2017-09-20 16:59:43 -0400

    updated processing for files for eyeIntegration

--- scripts/gene_pos_cell_type.R
+++ scripts/gene_pos_cell_type.R
@@ -2,16 +2,20 @@
 library(Seurat)
 library(tidyverse)
 library(here)
-# takes the macosko cell classifications and returns, for each gene, the positive counts (for the gene) in each of the 12 major cell types 
+library(RSQLite)
+# takes the macosko cell classifications and my processed (Seurat) macosko data and returns:
+# 1. for each gene, the positive counts (for the gene) in each of the 12 major cell types (gene_type_counts)
+# 2. a sqlite file with the individual sample counts for each of the 12 major cell types
+# 3. a file with the tsne coordinates 
 load('data/retina_seurat_superSet.Rdata')
 source('scripts/macosko_cluster_assignments.R')
 
 
-
+## 1
 all_genes <- retina_superset@data %>% rownames()
 gene_type_counts <- data.frame(c('Amacrine cells','Astrocytes','Bipolar cells','Cones','Fibroblasts','Horizontal cells','Microglia','Muller glia','Pericytes','Retinal ganglion cells','Rods','Vascular endothelium'))
 colnames(gene_type_counts) <- 'Cell Type'
-                               
+
 for (i in all_genes){
   gene_pos <- retina_superset@data[i,] > 0.1
   df <- retina_superset@meta.data[gene_pos,'Cell Type'] %>% table() %>% as.data.frame() %>% select(Freq)
@@ -23,3 +27,26 @@ for (i in all_genes){
 row.names(gene_type_counts) <- gene_type_counts$c..Amacrine.cells....Astrocytes....Bipolar.cells....Cones....Fibroblasts...
 gene_type_counts <- gene_type_counts[,-1]
 save(gene_type_counts, file='data/gene_type_counts.Rdata')
+
+
+## 2
+gene_count_long <- retina_superset@data %>% as.matrix() %>% data.frame() %>% rownames_to_column('Gene') %>% gather(`Cell ID`, `Gene Count`, -Gene) %>% filter(`Gene Count` > 0)
+metadata_long <-  retina_superset@meta.data %>% as.matrix() %>% data.frame() %>% rownames_to_column('Cell ID') %>% dplyr::select(`Cell ID`, `Cell.Type`, Macosko_Clusters) %>% mutate(`Cell Type` = Cell.Type) %>% select(-Cell.Type)
+gene_count_long_metadata <- left_join(gene_count_long, metadata_long) %>% filter(!is.na(`Cell Type`))
+sqlite_file <- 'data/single_cell_gene_counts.sqlite'
+sqldb <- dbConnect(SQLite(), dbname=sqlite_file)
+dbWriteTable(sqldb, 'single_cell_gene_counts', gene_count_long_metadata, field.types=NULL, over)
+dbGetQuery(sqldb, "CREATE INDEX GeneName on single_cell_gene_counts(Gene)")
+
+# 3
+tsne_coords <-GetDimReduction(object = retina, reduction.type = "tsne", slot = "cell.embeddings") %>% data.frame() %>% rownames_to_column('Cell ID') %>% 
+  left_join(metadata_long) %>% filter(!is.na(`Cell Type`))
+tsne_coords$Alpha = 0.03
+# example ggplot
+ggplot(tsne_coords, aes(x=tSNE_1,y=tSNE_2, colour=`Cell Type`))  + geom_point()
+# example labeling of samples with ZFP503 expression
+samples_gene_up <- colnames(retina_superset@data[,retina_superset@data['ABCA4',] > 5])
+ggplot(tsne_coords, aes(x=tSNE_1,y=tSNE_2, colour=`Cell Type`))  + 
+  geom_point(alpha=tsne_coords %>% mutate(Alpha = ifelse(`Cell ID` %in% samples_gene_up, 0.5, 0.03)) %>% pull(Alpha)) + 
+  xlab('tSNE 1') + ylab('tSNE 2') 
+
