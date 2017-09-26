c031a22b063564473d9dec1f2ffd548e328bd2d0
David McGaughey <mcgaughey@gmail.com>
2017-09-26 16:30:05 -0400

    moved chunk of code over to Human_eyeIntegration_paper repo

--- scripts/gene_pos_cell_type.R
+++ scripts/gene_pos_cell_type.R
@@ -4,9 +4,9 @@ library(tidyverse)
 library(here)
 library(RSQLite)
 # takes the macosko cell classifications and my processed (Seurat) macosko data and returns for eyeIntegration:
-# 1. a sqlite file with the individual sample counts for each of the 12 major cell types and metadata (subset and superset / short and long)
+# 1. a sqlite file with the individual sample counts for each of the 12 major cell types and metadata (subset and superset / short and long) and Gene names sql table
 # 2. a sql table with the tsne coordinates 
-# 3. Gene names sql table
+
 load('~/git/single_cell_retina/data/retina_seurat_superSet.Rdata')
 load('~/git/single_cell_retina/data/retina_seurat_subSet.Rdata')
 source('~/git/single_cell_retina/scripts/macosko_cluster_assignments.R')
@@ -20,7 +20,7 @@ gene_names <- gene_count_long$Gene %>% unique() %>% sort() %>% data.frame()
 colnames(gene_count_long) <- 'Gene'
 sqlite_file <- '~/git/Human_eyeIntegration_App/www/single_cell_retina_info.sqlite'
 sqldb <- dbConnect(SQLite(), dbname=sqlite_file)
-dbWriteTable(sqldb, 'single_cell_gene_counts', gene_count_long_metadata, field.types=NULL)
+dbWriteTable(sqldb, 'single_cell_gene_counts', gene_count_long_metadata, field.types=NULL, overwrite=TRUE)
 dbWriteTable(sqldb, 'single_cell_metadata_long', metadata_long, field.types=NULL)
 dbWriteTable(sqldb, 'single_cell_metadata_short', metadata_short, field.types=NULL)
 dbWriteTable(sqldb, 'gene_names', gene_names, field.types=NULL)
@@ -44,31 +44,4 @@ ggplot(tsne_coords, aes(x=tSNE_1,y=tSNE_2, colour=`Cell Type`))  +
   geom_point(alpha=tsne_coords %>% mutate(Alpha = ifelse(`Cell ID` %in% samples_gene_up, 0.5, 0.03)) %>% pull(Alpha)) + 
   xlab('tSNE 1') + ylab('tSNE 2') 
 
-# calculate ks p values for each gene against twelve cell types
-library(broom)
-cell_types <- c('Rods','Bipolar cells','Amacrine cells','Cones','Muller glia','Retinal ganglion cells','Vascular endothelium','Horizontal cells','Microglia','Pericytes','Astrocytes','Fibroblasts')
 
-cell_type_ids <- list()
-set.seed(1234)
-for (i in cell_types){
-  cell_type_ids[[i]] <- retina_superset@meta.data %>% data.frame() %>% rownames_to_column('Cell ID') %>% filter(`Cell.Type` == i) %>% pull(`Cell ID`)
-}
-
-gene_test <- 'ZFP503'
-
-cell_type_tester <- function(gene_name, cell_type_ids){
-  cell_type_test <- data.frame()
-  for (i in cell_types){
-    if (sum(retina_superset@data[gene_name,cell_type_ids[[i]]] > 0) > 0){
-      test_results <- wilcox.test(retina_superset@data[gene_name,retina_superset@data[gene_name,cell_type_ids[[i]]] > 0], 
-                             as.vector(retina_superset@data[,cell_type_ids[[i]]])) %>% 
-        tidy() %>% 
-        mutate(cell.type=i, gene=gene_name)
-        
-        cell_type_test <- rbind(test_results, cell_type_test)
-    }
-  }
-  return(cell_type_test)
-}
-
-cell_type_tester('ZFP503', cell_type_ids)
