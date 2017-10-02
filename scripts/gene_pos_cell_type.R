87f9713265e25bd68b5f3f64c3a22f063c66fbf8
David McGaughey <mcgaughey@gmail.com>
2017-10-02 16:46:55 -0400

    adding new stat for (a gene) how many cells of a cell type express that gene

--- scripts/gene_pos_cell_type.R
+++ scripts/gene_pos_cell_type.R
@@ -7,7 +7,8 @@ library(RSQLite)
 # 1. a sqlite file with the individual sample counts for each of the 12 major cell types and metadata (subset and superset / short and long) and Gene names sql table
 # 2. a sql table with the tsne coordinates 
 # 3. a sql table with summarized values for ratios of each of the 12 cell types by gene
-# 4. a sql table with mean expression of each gene by the 12 cell types and the ranks and deciles for each gene
+# 4. a sql table with, for each gene, the percentage of cells (split by cell type) expressing the gene
+# 5. a sql table with mean expression of each gene by the 12 cell types and the ranks and deciles for each gene
 
 load('~/git/single_cell_retina/data/retina_seurat_superSet.Rdata')
 load('~/git/single_cell_retina/data/retina_seurat_subSet.Rdata')
@@ -40,15 +41,34 @@ dbWriteTable(sqldb, 'tsne_coords', tsne_coords, field.types=NULL)
 cell_types <- c('Rods','Bipolar cells','Amacrine cells','Cones','Muller glia','Retinal ganglion cells','Vascular endothelium','Horizontal cells','Microglia','Pericytes','Astrocytes','Fibroblasts')
 gene_cell_type_perc <-gene_count_long_metadata %>% 
   group_by(Gene, `Cell Type`) %>% 
-  summarise(Count=n())%>% 
-  mutate(Percentage=(Count/sum(Count))*100) %>% 
+  summarise(`Cell Count`=n())%>% 
+  mutate(`Percentage Cell Types`=(`Cell Count`/sum(`Cell Count`))*100) %>% 
   left_join(data.frame(cell_types) %>% 
               select(`Cell Type`=cell_types),.,by=c('Cell Type'='Cell Type'))
 # calc rank and decile
-gene_cell_type_perc <- gene_cell_type_perc %>% arrange(`Cell Type`, -Percentage) %>% group_by(`Cell Type`) %>% mutate(Rank=row_number()) %>% mutate(Decile=ntile(-Rank, 10))
-dbWriteTable(sqldb, 'gene_cell_type_perc', gene_cell_type_perc, field.types=NULL, overwrite=TRUE)
+#gene_cell_type_perc <- gene_cell_type_perc %>% arrange(`Cell Type`, -Percentage) %>% group_by(`Cell Type`) %>% mutate(Rank=row_number()) %>% mutate(Decile=ntile(-Rank, 10))
+#dbWriteTable(sqldb, 'gene_cell_type_perc', gene_cell_type_perc, field.types=NULL, overwrite=TRUE)
 
 # 4
+cell_type_counts <- metadata_long %>% pull(`Cell Type`) %>% table() %>% data.frame()
+colnames(cell_type_counts) <- c('Cell Type', 'Total Count')
+cell_types <- c('Rods','Bipolar cells','Amacrine cells','Cones','Muller glia','Retinal ganglion cells','Vascular endothelium','Horizontal cells','Microglia','Pericytes','Astrocytes','Fibroblasts')
+cell_type_gene_perc <-gene_count_long_metadata %>% 
+  group_by(`Cell Type`, Gene) %>% 
+  summarise(`Cell Count`=n())%>% 
+  left_join(data.frame(cell_types) %>% 
+              select(`Cell Type`=cell_types),.,by=c('Cell Type'='Cell Type')) %>% 
+  left_join(., cell_type_counts) %>% 
+  mutate(`Percentage Cells` = (`Cell Count`/`Total Count`)*100)
+# calc rank and decile
+#cell_type_gene_perc <- cell_type_gene_perc %>% arrange(`Cell Type`, -Percentage) %>% group_by(`Cell Type`) %>% mutate(Rank=row_number()) %>% mutate(Decile=ntile(-Rank, 10)) 
+#dbWriteTable(sqldb, 'cell_type_perc', gene_cell_type_perc, field.types=NULL, overwrite=TRUE)
+# 5
+cell_type_ids <- list()
+set.seed(1234)
+for (i in cell_types){
+  cell_type_ids[[i]] <- retina_superset@meta.data %>% data.frame() %>% rownames_to_column('Cell ID') %>% filter(`Cell.Type` == i) %>% pull(`Cell ID`)
+}
 gene_means_by_type <- data.frame()
 for (i in cell_types){
   Mean <- apply(retina_superset@data[,cell_type_ids[[i]]], 1, function(x) mean(x))
@@ -57,9 +77,15 @@ for (i in cell_types){
   Mean$`Cell Type` <- i
   gene_means_by_type <- bind_rows(gene_means_by_type, Mean)
 }
-gene_means_by_type <- gene_means_by_type %>% arrange(`Cell Type`, -Mean) %>% group_by(`Cell Type`) %>% mutate(Rank=row_number()) %>% mutate(Decile=ntile(-Rank, 10)) %>% 
-  select(Gene, `Cell Type`, Mean, Rank, Decile)
-dbWriteTable(sqldb, 'gene_means_by_type', gene_means_by_type, field.types=NULL, overwrite=TRUE)
+#gene_means_by_type <- gene_means_by_type %>% arrange(`Cell Type`, -Mean) %>% group_by(`Cell Type`) %>% mutate(Rank=row_number()) %>% mutate(Decile=ntile(-Rank, 10)) %>% 
+#  select(Gene, `Cell Type`, Mean, Rank, Decile)
+#dbWriteTable(sqldb, 'gene_means_by_type', gene_means_by_type, field.types=NULL, overwrite=TRUE)
+gene_cell_type_stats <- left_join(gene_cell_type_perc, cell_type_gene_perc, by=c('Cell Type', 'Gene')) %>% 
+  right_join(. , gene_means_by_type, by=c('Cell Type', 'Gene'))
+
+## left_join 3,4,5 together
+
+## calc stat (rank/decile) for 3,4,5
 
 # disconnect database
 dbDisconnect(sqldb)
